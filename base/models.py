96da3a8faa848994f93a836bb8639ce48c087a4c
preciousimo <preciousimoniakemu@gmail.com>
2022-12-01 15:45:44 +0100

    Create models 💡

--- base/models.py
+++ base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=200)
+    view = models.DecimalField()
+    timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+class Content(models.Model):
+    photo = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=500, null=True, blank=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
