0bf76ed86a209398e9988c2b784e2e66aba6181a
preciousimo <preciousimoniakemu@gmail.com>
2022-12-01 15:48:49 +0100

    Fix models 💡

--- base/models.py
+++ base/models.py
@@ -3,8 +3,7 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     name = models.CharField(max_length=200)
-    view = models.DecimalField()
-    timestamp = models.DateTimeField()
+    subscriber = models.DecimalField()
 
     def __str__(self):
         return self.name
@@ -12,6 +11,8 @@ class Content(models.Model):
     photo = models.ImageField(null=True, blank=True)
     title = models.CharField(max_length=500, null=True, blank=True)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    view = models.DecimalField()
+    timestamp = models.DateTimeField()
 
     def __str__(self):
         return self.title
