617f209e9dcb1f6e707d33e8df23db7fb03159ab
preciousimo <preciousimoniakemu@gmail.com>
2022-12-02 16:39:50 +0100

    Adjust serialized models 💉

--- base/serializers.py
+++ base/serializers.py
@@ -5,11 +5,11 @@ from .models import Content, Profile
 class ProfileSerializer(ModelSerializer):
     class Meta:
         model = Profile
-        read_only_fields = '__all__'
+        fields = '__all__'
     
 
 class ContentSerializer(ModelSerializer):
     profile = ProfileSerializer()
     class Meta:
         model = Content
-        read_only_fields =  ['photo', 'title', 'profile']
+        fields = '__all__'
