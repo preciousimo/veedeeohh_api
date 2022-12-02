77b9dc6cf8e10aa09dbacfa0adda0fe70af17d4f
preciousimo <preciousimoniakemu@gmail.com>
2022-12-02 08:55:39 +0100

    Create temporary view 💡

--- base/views.py
+++ base/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse, Http404
+from django.db.models import Q
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .models import Content, Profile
+from .serializers import ContentSerializer, ProfileSerializer
+
 
 # Create your views here.
+@api_view(['GET'])
+def endpoints(request):
+
+    data = ['/profiles', '/contents', 'contents/:profile']
+    
+    return Response(data)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+# @permission_classes([IsAuthenticated])
+def content(request):
+    pass
+
+@api_view(['GET', 'PUT', 'DELETE'])
+# @permission_classes([IsAuthenticated])
+def profile_content(request):
+    pass
+
+@api_view(['GET', 'POST'])
+def profile(request):
+    pass
