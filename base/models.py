8e3fb710ca8a8ded932dd689265af3c54a0d48dc
preciousimo <preciousimoniakemu@gmail.com>
2022-12-02 17:54:55 +0100

    Make migrations to model 📂

--- base/models.py
+++ base/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
-    name = models.CharField(max_length=200) 
+    name = models.CharField(max_length=200)
+    img = models.ImageField(null=True, blank=True)
+
 
     def __str__(self):
         return self.name
