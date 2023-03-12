ade396695d0817c4e8dbf45ccb1f616903e2ea0b
preciousimo <preciousimoniakemu@gmail.com>
2023-03-12 22:02:34 +0100

    Customize Token Claims

--- authtokens/urls.py
+++ authtokens/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
+from rest_framework_simplejwt.views import ( 
     TokenRefreshView,
 )
 
