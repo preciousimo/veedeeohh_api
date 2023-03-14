50831dc3106310eed44b356fae1412ba8e8b3414
preciousimo <preciousimoniakemu@gmail.com>
2023-03-14 20:57:04 +0100

    Configure for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'video_api.wsgi'
--- video_api/settings.py
+++ video_api/settings.py
@@ -176,7 +176,7 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=4),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
+    "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
 
     "ALGORITHM": "HS256",
